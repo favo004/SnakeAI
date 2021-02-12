16b933b0af292fb448ad612397c8b3cf9c1bb01b
favo004 <jfavo@hotmail.com>
2021-02-12 14:07:52 -0500

    Added basic screen settings

---
+++ Snake/Config/ScreenSettings.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snake.Config
+{
+    public class ScreenSettings
+    {
+        private float gameWidth;
+        private float gameHeight;
+
+        public Rectangle ScreenBounds;
+
+        public ScreenSettings()
+        {
+            gameWidth = 960;
+            gameHeight = 540;
+
+            ScreenBounds = new Rectangle(0, 0, 960, 540);
+        }
+    }
+}
